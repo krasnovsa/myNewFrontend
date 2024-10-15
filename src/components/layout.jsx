813ce42157f6096021202a313aed7c246e03f042
krasnovsa <65534182+krasnovsa@users.noreply.github.com>
2024-10-15 17:04:43 +0300

    переделал разметку product page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,15 +8,15 @@ const Layout = ({
   className,
   children,
 }) => (
-  <div className="d-flex flex-column vh-100">
-    <header className="bg-primary text-white p-3" style={{ height: '15vh' }}>
+  <div className="d-flex flex-column vh-100 vw-100">
+    <header className="bg-primary text-white p-3" style={{ flex: '0 0 15vh' }}>
       <Menu />
       <div>
         <h1 className="display-4"> {title}</h1>
         <p className="lead"> {description}</p>
       </div>
     </header>
-    <main className={`flex-grow-1 overflow-auto ${className}`} style={{ height: '90vh' }}>
+    <main className={`flex-grow-1 ${className}`} style={{ overflow: 'hidden' }}>
       {children}
     </main>
   </div>
--- src/pages/product-page/product-page.jsx
+++ src/pages/product-page/product-page.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import Split from "react-split";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css"; // Подключим кастомные стили
 import Layout from "../../components/layout.jsx";
 import TpAddForm from "../../components/tp/tp-add-form/tp-add-form.jsx";
 import TpListForm from "../../components/tp/tp-list-form/tp-list-form.jsx";
 import { getProductById } from "../../api/apiProduct";
-import { isAuthenticated } from "../../auth";
-import { CurrentAppContext } from "../../contexts/currentApp";
+import { isAuthenticated } from "../../auth/index.js";
+import { CurrentAppContext } from "../../contexts/currentApp.jsx";
 
 function ProductPage() {
   const [prodId, setProdId] = useState("");
@@ -36,17 +35,9 @@ function ProductPage() {
   return (
     <div className="main-container">
       <Layout title="Управление продукцией" description="Управляем продукцией">
-        <Split
-          sizes={[40, 60]} // Начальные размеры 40% и 60% для первой и второй панели
-          minSize={[600, 600]} // Минимальная ширина в пикселях для левой и правой панели
-          maxSize={[Infinity, Infinity]} // Максимальная ширина для левой панели, правая может расширяться бесконечно
-          direction="horizontal"
-          style={{ display: "flex", height: "100%" }}
-        >
-          <div className="pane">
-            <TpAddForm prodId={state.curProduct ? state.curProduct.Id : 0} />
-          </div>
-          <div className="pane">
+        <div className="grid-container">
+          <div className="grid-item header">
+            <h1>Верхняя левая панель</h1>
             <div className="prod-id-input">
               <label htmlFor="prodId">Product ID:</label>
               <input
@@ -55,10 +46,19 @@ function ProductPage() {
                 value={prodId}
                 onChange={handleProdIdChange}
               />
-            </div>
+            </div>    
+          </div>
+          <div className="grid-item header">
+            <h1>Верхняя правая панель</h1>
+          </div>
+          <div className="grid-item left-pane">
+            <TpAddForm prodId={state.curProduct ? state.curProduct.Id : 0} />
+          </div>
+          <div className="grid-item right-pane">
+
             <TpListForm prodId={prodId} />
           </div>
-        </Split>
+        </div>
       </Layout>
     </div>
   );
