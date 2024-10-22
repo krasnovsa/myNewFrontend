72ec4682bb70467f5b4211ab64a6f4d3f33031a9
krasnovsa <krasnovsa.tm@gmail.com>
2024-10-22 13:27:56 +0300

    переделываю выбор материала

--- src/components/material/material-select-by-params/material-select-by-params.jsx
+++ src/components/material/material-select-by-params/material-select-by-params.jsx
@@ -1,16 +1,25 @@
-import React, { useEffect, useState, useCallback } from "react";
-import {
-  getMaterialById,
-  getMaterialProfileTypes,
-  findMaterialByParams,
-} from "../../../api/apiMaterial";
+import React, { useEffect, useState } from "react";
+import { getMaterialProfileTypes } from "../../../api/apiMaterial";
 import { getMaterialMarksList } from "../../../api/apiMatMarks"; // Импортируем метод для получения списка марок материала
 import { isAuthenticated } from "../../../auth/index";
 import "./styles.css"; // Подключаем стили
-import calculateMaterialProperties from "../calc-mat-mass"; // Импортируем функцию расчета массы
+import MaterialFindByParams from "./MaterialFindByParams"; // Импортируем дочерний компонент
 
-const MaterialSelectByParams = ({ matId, handleSelectPopup }) => {
-  const [materialData, setMaterialData] = useState({});
+const MaterialSelectByParams = ({
+  dim1: initialDim1,
+  dim2: initialDim2,
+  dim3: initialDim3,
+  profile: initialProfile,
+  density: initialDensity,
+  handleSelectPopup,
+}) => {
+  const [materialData, setMaterialData] = useState({
+    dim1: initialDim1,
+    dim2: initialDim2,
+    dim3: initialDim3,
+    profile: initialProfile,
+    density: initialDensity,
+  });
   const [materialMarks, setMaterialMarks] = useState([]);
   const [profileTypes, setProfileTypes] = useState([]);
   const [dimensionLabels, setDimensionLabels] = useState({
@@ -23,30 +32,23 @@ const MaterialSelectByParams = ({ matId, handleSelectPopup }) => {
     dim2: true,
     dim3: true,
   });
-  const [foundMaterial, setFoundMaterial] = useState(null);
   const { user, token } = isAuthenticated();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [materialData, marks, profiles] = await Promise.all([
-          getMaterialById(matId, user._id, token),
+        const [marks, profiles] = await Promise.all([
           getMaterialMarksList(user._id, token),
           getMaterialProfileTypes(user._id, token),
         ]);
-        console.log("Полученные данные о материале:", materialData);
-        setMaterialData(materialData);
         setMaterialMarks(marks);
         setProfileTypes(profiles);
       } catch (err) {
         console.error("Ошибка при получении данных:", err);
       }
     };
-
-    if (matId) {
-      fetchData();
-    }
-  }, [matId, user._id, token]);
+    fetchData();
+  }, [user._id, token]);
 
   useEffect(() => {
     if (materialData.profile) {
@@ -68,94 +70,7 @@ const MaterialSelectByParams = ({ matId, handleSelectPopup }) => {
     }
   }, [materialData.profile, profileTypes]);
 
-  const searchMaterial = useCallback(
-    async (params) => {
-      try {
-        console.log("Поиск материала по параметрам", params);
-        const result = await findMaterialByParams(user._id, token, params);
-
-        if (result.length > 0) {
-          console.log("Найден материал:", result[0]);
-          setFoundMaterial(result[0]);
-          setMaterialData((prevData) => ({
-            ...prevData,
-            mass1m: result[0].mass1m,
-          }));
-        } else {
-          console.log("Такого материала нет");
-          // Рассчитываем массу 1 метра и имя нового материала
-          const calculated = calculateMaterialProperties(
-            params.dim1,
-            params.dim2,
-            params.dim3,
-            params.profile,
-            materialMarks.find((mark) => mark.Id == params.markId)?.density ||
-              0,
-            materialMarks.find((mark) => mark.Id == params.markId)?.name || "",
-            params.qlt
-          );
-
-          setFoundMaterial({
-            ...calculated,
-            dim1: params.dim1,
-            dim2: params.dim2,
-            dim3: params.dim3,
-            profile: params.profile,
-            density:
-              materialMarks.find((mark) => mark.Id == params.markId)?.density ||
-              0,
-            markName:
-              materialMarks.find((mark) => mark.Id == params.markId)?.name ||
-              "",
-            qlt: params.qlt,
-            Id: 0,
-          });
-
-          setMaterialData((prevData) => ({
-            ...prevData,
-            mass1m: calculated.mass1m,
-          }));
-        }
-      } catch (err) {
-        console.error("Ошибка при поиске материала:", err);
-        setFoundMaterial("Ошибка при поиске материала");
-        setMaterialData((prevData) => ({
-          ...prevData,
-          mass1m: 0,
-        }));
-      }
-    },
-    [user._id, token, materialMarks]
-  );
-
-  useEffect(() => {
-    const initialSearch = async () => {
-      const params = {
-        markId: materialData.markId,
-        dim1: materialData.dim1,
-        dim2: materialData.dim2 || 0,
-        dim3: materialData.dim3 || 1,
-        qlt: materialData.qlt || null,
-        profile: materialData.profile,
-      };
-
-      if (materialData.markId && materialData.dim1 && materialData.profile) {
-        await searchMaterial(params);
-      }
-    };
-
-    initialSearch();
-  }, [
-    materialData.markId,
-    materialData.dim1,
-    materialData.profile,
-    searchMaterial,
-    materialData.dim2,
-    materialData.dim3,
-    materialData.qlt,
-  ]);
-
-  const handleInputChange = async (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     setMaterialData((prevData) => ({
       ...prevData,
@@ -183,53 +98,6 @@ const MaterialSelectByParams = ({ matId, handleSelectPopup }) => {
           dim3: 1,
         }));
       }
-    }
-
-    // Направляем запрос findMaterialByParams при изменении значений
-    const params = {
-      markId: name === "markId" ? value : materialData.markId,
-      dim1: name === "dim1" ? value : materialData.dim1,
-      dim2: name === "dim2" ? value : materialData.dim2 || 0,
-      dim3: name === "dim3" ? value : materialData.dim3 || 1,
-      qlt:
-        name === "qlt"
-          ? value === ""
-            ? null
-            : value
-          : materialData.qlt || null,
-      profile: name === "profile" ? value : materialData.profile,
-    };
-
-    console.log("params", params);
-
-    await searchMaterial(params);
-
-    // Обновляем подписи к размерам и видимость полей при изменении профиля
-  };
-
-  const handleMass1mChange = (e) => {
-    const { value } = e.target;
-    setFoundMaterial((prevData) => ({
-      ...prevData,
-      mass1m: value,
-    }));
-  };
-
-  const handleCreateMaterial = () => {
-    // Логика создания нового материала
-    console.log("Создать новый материал");
-  };
-
-  if (!matId) {
-    return null;
-  }
-
-  const handleSelectMaterial = () => {
-    console.log("Выбран материал:", foundMaterial);
-    if (foundMaterial && foundMaterial.Id) {
-      handleSelectPopup(foundMaterial.Id, false);
-    } else {
-      console.error("Ошибка: foundMaterial не содержит Id");
     }
   };
 
@@ -356,50 +224,19 @@ const MaterialSelectByParams = ({ matId, handleSelectPopup }) => {
               id="mass1m"
               name="mass1m"
               className="form-control"
-              value={
-                foundMaterial && foundMaterial !== "Такого материала нет"
-                  ? foundMaterial.mass1m
-                  : 0
-              }
-              onChange={handleMass1mChange}
+              value={materialData.mass1m || 0}
+              readOnly
             />
           </div>
         </form>
 
-        {foundMaterial &&
-          foundMaterial.matName && ( // matName взяли из calculateMaterialProperties
-            <>
-              <p style={{ color: "red" }}>
-                Материал <strong>{foundMaterial.matName} </strong> не найден
-              </p>
-              <div className="mt-3">
-                <p>Масса 1м: {foundMaterial.mass1m} кг</p>
-              </div>
-              <button
-                className="btn btn-primary mt-3"
-                onClick={handleCreateMaterial}
-              >
-                Создать материал
-              </button>
-            </>
-          )}
-        {foundMaterial &&
-          foundMaterial.name && ( // matName взяли из calculateMaterialProperties
-            <>
-              <p style={{ color: "green" }}>
-                Материал <strong>{foundMaterial.name} </strong> найден
-              </p>
-              <div className="mt-3">
-                <p>Масса 1м: {foundMaterial.mass1m} кг</p>
-              </div>
-              <button
-                className="btn btn-primary btn-success mt-3"
-                onClick={handleSelectMaterial}
-              >
-                Выбрать
-              </button>
-            </>
-          )}
+        <MaterialFindByParams
+          dim1={materialData.dim1}
+          dim2={materialData.dim2}
+          dim3={materialData.dim3}
+          profile={materialData.profile}
+          density={materialData.density}
+        />
       </div>
     </div>
   );
